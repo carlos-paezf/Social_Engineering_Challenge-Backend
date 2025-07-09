717a79e888d7c58d6f85d45e4fcabe50bdcd1f8f
Carlos David Páez Ferreira <cpaezferreira@gmail.com>
2025-07-09 15:08:22 -0500

    feat: add hint module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { GroupModule } from './group/group.module';
 import { AuthModule } from './auth/auth.module';
 import { RankingModule } from './ranking/ranking.module';
+import { HintModule } from './hint/hint.module';
 
 @Module({
-  imports: [GroupModule, AuthModule, RankingModule],
+  imports: [GroupModule, AuthModule, RankingModule, HintModule],
   controllers: [AppController],
   providers: [AppService],
 })
