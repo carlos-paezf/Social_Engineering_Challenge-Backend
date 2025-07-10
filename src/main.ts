1b4f4fe167d7ef335ccbbd0ded727fdd255a5dc8
Carlos David Páez Ferreira <cpaezferreira@gmail.com>
2025-07-10 15:40:56 -0500

    feat: update CORS configuration to allow requests from additional origins

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap () {
   const app = await NestFactory.create( AppModule );
   app.setGlobalPrefix( "api" );
   app.enableCors( {
-    origin: [ 'http://localhost:5173', "https://carlos-paezf.github.io/" ],
+    origin: [ 'http://localhost:5173', "https://carlos-paezf.github.io", "0.0.0.0" ],
     credentials: true
   } );
   await app.listen( process.env.PORT ?? 3001 );
