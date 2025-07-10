6f9795ae20ce7ff90acb3252746bc9daf91279a1
Carlos David Páez Ferreira <cpaezferreira@gmail.com>
2025-07-10 15:35:52 -0500

    feat: update CORS configuration to allow requests from GitHub Pages

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap () {
   const app = await NestFactory.create( AppModule );
   app.setGlobalPrefix( "api" );
   app.enableCors( {
-    origin: [ 'http://localhost:5173' ],
+    origin: [ 'http://localhost:5173', "https://carlos-paezf.github.io/" ],
     credentials: true
   } );
   await app.listen( process.env.PORT ?? 3001 );
